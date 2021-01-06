55ca6a3df5896f5869141d29835abe27023647ef
James Brandt <westred22@hotmail.com>
2021-01-06 02:07:41 -0800

    Flask madlibs- crete your own story

---
+++ app.py
@@ -0,0 +1,20 @@
+from flask import Flask, render_template, request
+from flask_debugtoolbar import DebugToolbarExtension
+from stories import story
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = "secret"
+debug = DebugToolbarExtension(app)
+
+
+# Here is the template
+@app.route("/")
+def ask_questions():
+    prompts = story.prompts
+    return render_template("base.html", prompts=prompts)
+    
+@app.route('/story')
+def gen_story():
+    text = story.generate(request.args)
+
+    return render_template("story.html", text=text)
